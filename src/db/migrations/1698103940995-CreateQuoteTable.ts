57306551552b51b7430391bc80b36623c429e136
Theo Nejm <54482697+theo-nejm@users.noreply.github.com>
2024-06-07 01:51:35 -0300

    feat: configure postgres + migrations (#4)

--- src/db/migrations/1698103940995-CreateQuoteTable.ts
+++ src/db/migrations/1698103940995-CreateQuoteTable.ts
@@ -5,11 +5,18 @@ export class CreateQuoteTable1698103940995 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`queue\` (\`id\` varchar(36) NOT NULL, \`title\` varchar(63) NOT NULL, \`description\` varchar(255) NOT NULL, \`average_wait_time_in_minutes\` smallint NOT NULL, \`max_participants\` smallint NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE "queue" (
+        "id" uuid NOT NULL,
+        "title" varchar(63) NOT NULL,
+        "description" varchar(255) NOT NULL,
+        "average_wait_time_in_minutes" smallint NOT NULL,
+        "max_participants" smallint NOT NULL,
+        CONSTRAINT "PK_queue_id" PRIMARY KEY ("id")
+      )`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`queue\``);
+    await queryRunner.query(`DROP TABLE "queue"`);
   }
 }
